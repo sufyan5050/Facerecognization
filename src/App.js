56352ab81e6e64165b1331dc33de76a3e4190fa8
sufyan5050 <skhelpp@gmail.com>
2022-01-13 05:36:33 -0800

    update app

---
+++ src/App.js
@@ -0,0 +1,113 @@
+import React,{Component} from 'react';
+import Navigation from './Navigation/Navigation';
+import Logo from './Logo/Logo';
+import ImageLinkForm from './ImageLinkForm/ImageLinkForm';
+import Rank from './Rank/Rank';
+import Particles from "react-tsparticles";
+
+
+import './App.css';
+const particlesInit = (main) => {
+  console.log(main);
+
+  // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+};
+
+const particlesLoaded = (container) => {
+  console.log(container);
+};
+
+class App extends Component{
+  
+  render(){
+  return (
+    <div className="App">
+   <Particles className='particles'
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        fpsLimit: 60,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+          modes: {
+            bubble: {
+              distance: 400,
+              duration: 2,
+              opacity: 0.8,
+              size: 40,
+            },
+            push: {
+              quantity: 4,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#ffffff",
+          },
+          links: {
+            color: "#ffffff",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outMode: "bounce",
+            random: false,
+            speed: 6,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            random: true,
+            value: 5,
+          },
+        },
+        detectRetina: true,
+      }}
+    />
+      <Navigation/>
+       <Logo/>
+       <Rank/>
+      <ImageLinkForm/>
+      {/*<Facerec/>*/}
+    </div>
+  );
+}
+}
+
+
+export default App;
---
+++ src/Rank/Rank.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+const Rank = () => {
+    return(
+      <div>
+      <div className='white f3'>
+      {'Sufyan, Your cruent rank is ....'}
+      <div className='white f1'>
+      {'#5'}
+      </div>
+
+      </div>
+         
+      </div>
+    );
+}
+
+export default Rank;
